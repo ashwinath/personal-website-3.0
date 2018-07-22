2c889bc7b0ff2e9409f9ede94f7c5d0c8ed43940
ashwinath <ashwinath@hotmail.com>
2018-07-22 21:38:32 +0800

    fix: no check ssh host

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh travis@ashwinchat.com -t "docker pull ashwinath/personal-website-3 && docker stop website && docker rm website && docker run --name website -d -p 8080:8080 ashwinath/personal-website-3"
+ssh -o "StrictHostKeyChecking no" travis@ashwinchat.com -t "docker pull ashwinath/personal-website-3 && docker stop website && docker rm website && docker run --name website -d -p 8080:8080 ashwinath/personal-website-3"
