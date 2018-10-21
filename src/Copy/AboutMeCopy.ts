3675c71624efb341d8d80bd4ad7889c2324e42e8
Ashwin Chatterji <ashwinath@hotmail.com>
2018-10-21 14:50:31 +0800

    feat: added copy

--- src/Copy/AboutMeCopy.ts
+++ src/Copy/AboutMeCopy.ts
@@ -6,40 +6,40 @@ export const contentHeaderProps = {
 
 export const aboutMeProps = {
   imageUrl: "./Images/ProfilePicture.jpg",
-  title: "Hello there.",
+  title: "Hello there",
   subtitle: "Software Engineer @ 99.co",
   text: [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
+    "I graduated with a marketing degree but have been working professionally as a software engineer. Currently, I work at 99.co as a software engineer.",
   ],
 }
 
 export const workExperienceProps = [
   {
     imageUrl: "./Images/NinetyNine.png",
-    title: "Backend Engineer",
-    subtitle: "Software Engineer @ 99.co",
+    title: "99.co",
+    subtitle: "Software Engineer",
     text: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
+      "Primarily a backend engineer, but have been involved a lot in operations, and a bit of frontend. I mainly develop and augment the product with more features here.",
+      "I have worked on the core feature of the the website, search, which is powered by Elasticsearch. I wrote most of the New Launches search feature back in September 2018. I have also worked heavily on the advertising platform of the company which is called Maximiser, both in the logic and implementation of the feature. More info about my learnings can be found in my blog.",
+      "I take pride in what I do and have extremely high code coverage in the services I write."
     ],
   },
   {
-    imageUrl: "./Images/NinetyNine.png",
-    title: "Backend Engineer",
-    subtitle: "Software Engineer @ 99.co",
+    imageUrl: "./Images/HPEDXC.png",
+    title: "HPE/DXC Technology",
+    subtitle: "Software Engineer",
     text: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
+      "First job as a software engineer taking on a piece of mission critical softare in the enterprise world. I learnt most of the basics of programming here.",
+      "While the technology is not very bleeding edge here, it presents a challenge of using technology that have been used in the past and has taught me to read and work on legacy code. There is a greater appreciation of the tools I use on my side projects and my current job now."
     ],
   },
   {
-    imageUrl: "./Images/NinetyNine.png",
-    title: "Backend Engineer",
-    subtitle: "Software Engineer @ 99.co",
+    imageUrl: "./Images/Epikk.png",
+    title: "Epikk Co",
+    subtitle: "Content Marketer",
     text: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id nulla eros. In iaculis dolor in orci condimentum, vestibulum porttitor velit commodo. Sed nec pulvinar dui, et faucibus elit. Proin. ",
+      "Interned as a content marketer. Wrote articles for the company blog, talked to the maker community and wrote articles about them.",
+      "This was when I did not have any knowledge of programming. I decided not to stay in marketing as it doesn't interest me. This was the turning point of transitioning into a software engineer."
     ],
   },
 ]
